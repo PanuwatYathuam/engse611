b6e1737962b048a2112bb5ce0ed6b2c134e6f604
panuwatYathuam <panuwat_ya67@live.rmutl.ac.th>
2025-01-27 09:25:53 +0700

    lab5

---
+++ lab5/01-variables-data-types/01-console/script.js
@@ -0,0 +1,10 @@
+console.log("Morning!");
+console.log(100);
+console.log('A');
+console.log(98.25);
+console.log(5*4);
+console.log("Morning " + "EUREKA");
+const x = 100;
+console.log(x);
+console.error('Alert');
+console.warn('Warning');
